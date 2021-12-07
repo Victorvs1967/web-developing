640fabd3ac989cbf849590790e16f64cd0d08079
Victor <victorsmirnov@me.com>
2021-12-07 22:53:23 +0200

    lesson 12+

--- brandshop/index.php
+++ brandshop/index.php
@@ -43,7 +43,7 @@
         [
             'id' => 3,
             'name' => 'Samsung',
-            'desc' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque possimus corporis dolor eos in laborum harum, facere magni consequuntur quisquam, sint maiores molestias voluptatem nihil eaque. Quo qui ad nihil!',
+            'desc' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque possimus corporis dolor eos in laborum harum',
             'img' => 'images/goods/samsung.jpg',
             'price' => '$1200'
         ],
