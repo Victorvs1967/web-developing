89c429424561acb468a81f0eb76d98d6a9239137
Victor <victorsmirnov@me.com>
2021-12-07 23:20:46 +0200

    lesson 12+++

---
+++ brandshop/data/goods.php
@@ -0,0 +1,24 @@
+<?php
+    $goods = [
+        [
+            'id' => 1,
+            'name' => 'iPhone',
+            'desc' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque possimus corporis dolor eos in laborum harum, facere magni consequuntur quisquam, sint maiores molestias voluptatem nihil eaque. Quo qui ad nihil!',
+            'img' => 'images/goods/iphone.jpg',
+            'price' => '$2000'
+        ],
+        [
+            'id' => 2,
+            'name' => 'HTC',
+            'desc' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque possimus corporis dolor eos in laborum harum, facere magni consequuntur quisquam, sint maiores molestias voluptatem nihil eaque. Quo qui ad nihil!',
+            'img' => 'images/goods/htc.jpg',
+            'price' => '$1000'
+        ],
+        [
+            'id' => 3,
+            'name' => 'Samsung',
+            'desc' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque possimus corporis dolor eos in laborum harum',
+            'img' => 'images/goods/samsung.jpg',
+            'price' => '$1200'
+        ],
+    ];
